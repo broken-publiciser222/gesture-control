113d6f705a38a1cd538a3b45dd3bbd0d7b56a4ad
alex6712 <alexeivanyukov@yandex.ru>
2026-03-21 13:05:38 +0300

    Первичное объединение cmd и internal

--- clients/cli/cmd/root.go
+++ clients/cli/cmd/root.go
@@ -13,6 +13,7 @@ var rootCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Hello, 世界!")
 	},
+	SilenceUsage: true,
 }
 
 func Execute() {
--- clients/cli/cmd/start.go
+++ clients/cli/cmd/start.go
@@ -4,14 +4,22 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+
+	"cli/internal/ffmpeg"
 )
 
 var startCmd = &cobra.Command{
 	Use:   "start",
 	Short: "Запустить управление с помощью жестов",
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if _, err := ffmpeg.Find(); err != nil {
+			return err
+		}
+
 		fmt.Println("Утилита запущена...")
+		return nil
 	},
+	SilenceErrors: true,
 }
 
 func init() {
